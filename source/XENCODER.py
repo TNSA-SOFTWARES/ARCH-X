7ac8422ec925ef13312c6b2b6213265fbe951333
TNSA <128797664+TNSA-SOFTWARES@users.noreply.github.com>
2024-04-17 17:39:05 +0530

    Update XENCODER.py

--- source/XENCODER.py
+++ source/XENCODER.py
@@ -1,11 +1,1 @@
-import tensorflow as tf
-from transformer import Encoder, Decoder
 
-# Define XEncoder class
-class XEncoder(tf.keras.layers.Layer):
-    def __init__(self, num_layers, d_model, num_heads, dff, input_vocab_size, maximum_position_encoding, rate=0.1):
-        super(XEncoder, self).__init__()
-        self.encoder = Encoder(num_layers, d_model, num_heads, dff, input_vocab_size, maximum_position_encoding, rate)
-
-    def call(self, inputs, training, mask):
-        return self.encoder(inputs, training, mask)
